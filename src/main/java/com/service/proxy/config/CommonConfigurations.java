38042fc77650294e8bc912a3302cc31582f37520
748152 <748152@PC443683.cts.com>
2019-04-30 19:54:57 +0530

    Architechture as per the requirement.

--- src/main/java/com/service/proxy/config/CommonConfigurations.java
+++ src/main/java/com/service/proxy/config/CommonConfigurations.java
@@ -1,23 +1,21 @@
 package com.service.proxy.config;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class CommonConfigurations {
 	
 	private String methodType;
+	private String endpoint;
+	private List<String> allowedQueryParams=new ArrayList<String>(); 
 	private String inputSchemaFile; 
     private String outputSchemaFile;
 	private Boolean inputValidationRequired;
     private Boolean outputValidationRequired;
     private String requiredHeaders[];
+    private List<SecurityHeaders> securityHeaders=new ArrayList<SecurityHeaders>();
     
-	@Override
-	public String toString() {
-		return "          CommonConfigurations [methodType=" + methodType + ", inputSchemaFile=" + inputSchemaFile
-				+ ", outputSchemaFile=" + outputSchemaFile + ", inputValidationRequired=" + inputValidationRequired
-				+ ", outputValidationRequired=" + outputValidationRequired + ", requiredHeaders="
-				+ Arrays.toString(requiredHeaders) + "]";
-	}
 	public String getMethodType() {
 		return methodType;
 	}
@@ -54,6 +52,54 @@ public class CommonConfigurations {
 	public void setRequiredHeaders(String[] requiredHeaders) {
 		this.requiredHeaders = requiredHeaders;
 	}
-    
-    
+	public String getEndpoint() {
+		return endpoint;
+	}
+	public void setEndpoint(String endpoint) {
+		this.endpoint = endpoint;
+	}
+	public List<String> getAllowedQueryParams() {
+		return allowedQueryParams;
+	}
+	public void setAllowedQueryParams(List<String> allowedQueryParams) {
+		this.allowedQueryParams = allowedQueryParams;
+	}
+	public List<SecurityHeaders> getSecurityHeaders() {
+		return securityHeaders;
+	}
+	public void setSecurityHeaders(List<SecurityHeaders> securityHeaders) {
+		this.securityHeaders = securityHeaders;
+	}
+	@Override
+	public String toString() {
+		return "CommonConfigurations [methodType=" + methodType + ", endpoint=" + endpoint + ", allowedQueryParams="
+				+ allowedQueryParams + ", inputSchemaFile=" + inputSchemaFile + ", outputSchemaFile=" + outputSchemaFile
+				+ ", inputValidationRequired=" + inputValidationRequired + ", outputValidationRequired="
+				+ outputValidationRequired + ", requiredHeaders=" + Arrays.toString(requiredHeaders)
+				+ ", securityHeaders=" + securityHeaders + "]";
+	}
+	
+}
+
+class SecurityHeaders
+{
+	private String headername;
+	private String headervalue;
+	
+	public String getHeadername() {
+		return headername;
+	}
+	public void setHeadername(String headername) {
+		this.headername = headername;
+	}
+	public String getHeadervalue() {
+		return headervalue;
+	}
+	public void setHeadervalue(String headervalue) {
+		this.headervalue = headervalue;
+	}
+	@Override
+	public String toString() {
+		return "SecurityHeaders [headername=" + headername + ", headervalue=" + headervalue + "]";
+	}
 }
